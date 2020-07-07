ae34388f47849772a07b439afd2f349bb088b674
ZiqiangYoung <ziqiangyoung@foxmail.com>
2020-07-08 00:00:00 +0800

    Hello World!

---
+++ src/main/java/ziqiang/Main.java
@@ -0,0 +1,14 @@
+package ziqiang;
+
+/**
+ * <strong>TODO:Class Doc required</strong>
+ * <p>Created by ZiqiangYoung on 2023/4/10</p>
+ *
+ * @author ZiqiangYoung, ziqiangyoung@foxmail.com
+ * @version V0.1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
